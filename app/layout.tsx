f8661733c1a4850f9d869d33aae0336142c2cf4b
Sanskar Modgill <sanskarmodgill@gmail.com>
2026-08-16 18:25:44 +0530

    Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter, Playfair_Display } from "next/font/google";
+import { Montserrat, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import WhatsAppButton from "../components/WhatsAppButton";
 
-const inter = Inter({
-  variable: "--font-inter",
+const Montse = Montserrat({
+  variable: "--font-montse",
   subsets: ["latin"],
 });
 
@@ -57,7 +57,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${inter.variable} ${Playfair_display.variable} h-full antialiased`}
+      className={`${Montse.variable} ${Playfair_display.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-[var(--cream)] text-[var(--slate)]">
         <Navbar />
